0f0b8d32787b81553b1a24bd9fad673208133518
yagnesh2004 <93670699+yagnesh2004@users.noreply.github.com>
2023-02-22 17:30:27 +0530

    Heads or tails project started

---
+++ Toss.js
@@ -0,0 +1,26 @@
+const element = document.getElementById('button')
+element.addEventListener("click",myTossResult);
+
+
+function myTossResult() {
+    var randomNumber = Math.floor(Math.random() *2 +1)
+console.log(randomNumber)
+
+var randomImage = "img" + randomNumber + ".png";
+var imageSource = "images/" + randomImage ;
+
+var imageOriginal = document.querySelectorAll('img')[0];
+
+imageOriginal.setAttribute('src',imageSource)
+
+if ( randomNumber == 1){
+    document.querySelector('h1').innerHTML ="Head"
+}
+else{
+    document.querySelector('h1').innerHTML = "Tail"
+}
+
+
+}
+
+
